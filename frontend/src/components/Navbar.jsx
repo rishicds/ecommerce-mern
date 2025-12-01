a4d89ba2c78c61674ebf5d8a5667113c9ea9483f
Pragya79645 <pragya220898@gmail.com>
2025-12-01 19:04:00 +0530

    .

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -315,7 +315,7 @@ const Navbar = () => {
                         >
                             <svg
                                 xmlns="http://www.w3.org/2000/svg"
-                                className="h-5 w-5 sm:h-6 sm:w-6"
+                                className="h-6 w-6 sm:h-7 sm:w-7"
                                 fill={wishlistCount > 0 ? "#FFB81C" : "none"}
                                 viewBox="0 0 24 24"
                                 stroke="currentColor"
